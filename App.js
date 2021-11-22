781b53fda8fe7f23f55d559d334803e670a5b4bd
Victor Alencar <87835966+VictorAlencar1@users.noreply.github.com>
2021-11-21 23:09:58 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aprendendo React</h1>
+      <img src="https://lh3.googleusercontent.com/proxy/U5KQh7uSUeVXpEvpQNyg5BzNvG6qDIgYF1YTeGs96A-baVXJB9qNuYtYz7KPOzJ_xAs59balQuccp8l371Svvzm79iNOSeiU2oP1DryOqfHVSW365Uj2I2PkOyq_zEmQELE53oyETEs"/>
+      <pre>
+      Por influência da mitologia grega, tanto que Atena, deusa da guerra e da sabedoria,<br/>
+      tinha uma coruja como mascote. Os gregos consideravam a noite como o momento do <br/>
+      pensamento filosófico e da revelação intelectual e a coruja, por ser uma ave noturna,<br/>
+      acabou representando essa busca pelo saber.<br/>
+      </pre>
+    </div>
+  );
+}
+
+export default App;
